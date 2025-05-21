0e189d644a87bfcb271f92d38bba41c8865209c6
Ashirwada Manamudali <91813126+zionashirwada@users.noreply.github.com>
2025-05-21 17:10:30 +0530

    Update ActivitiesCard.tsx

--- src/components/dashboard/ActivitiesCard.tsx
+++ src/components/dashboard/ActivitiesCard.tsx
@@ -11,22 +11,24 @@ import CardHeader from "./CardHeader";
 const tabData = {
   user: {
     stats: [
-      { title: "Active Users", value: "237", percentage: "40%", isPositive: true, tooltip: "The number of users who have logged into the platform within the selected timeframe." },
-      { title: "New Users", value: "8", percentage: "40%", isPositive: false, tooltip: "The number of new users who have registered to the platform within that time range" }
-    ],
+       { title: "Active Users", value: "237", percentage: "40%", isPositive: true, tooltip: "The number of users who have logged into the platform within the selected timeframe." },
+       { title: "Signups", value: "8", percentage: "40%",  isPositive: true, tooltip: "The number of new users who have registered to the platform within that time range" }
+      ],
     chartType: 'user' as TabType
   },
   usage: {
     stats: [
-      { title: "Total Usage", value: "1,243", percentage: "15%", isPositive: true, tooltip: "Total Number of Hours spent on the platform by users" },
-      { title: "Avg. Time", value: "45m", percentage: "5%", isPositive: true, tooltip: "Average time spent on the platform by users" }
+      { title: "Platform", value: "1,243h", percentage: "15%", isPositive: true, tooltip: "Total Number of Hours spent on the platform by users" },
+      { title: "Course", value: "789h", percentage: "8%", isPositive: true, tooltip: "Total Number of Hours spent on Courses by users" },
+      { title: "Library", value: "412h", percentage: "5%", isPositive: false, tooltip: "Total Number of Hours spent on Courses by users" }
     ],
     chartType: 'usage' as TabType
   },
   course: {
     stats: [
-      { title: "Completed", value: "89", percentage: "12%", isPositive: false, tooltip: "Number of learners who completed Courses" },
-      { title: "In Progress", value: "156", percentage: "32%", isPositive: true, tooltip: "Number of learners assigned for Courses" }
+      { title: "Assigned", value: "200", percentage: "18%", isPositive: true, tooltip: "Number of learners assigned for Courses" },
+      { title: "Passed", value: "134", percentage: "9%", isPositive: true, tooltip: "Number of learners who achieved a passing score" },
+      { title: "Completed", value: "89", percentage: "12%", isPositive: false, tooltip: "Number of learners who completed Courses" }
     ],
     chartType: 'course' as TabType
   }
