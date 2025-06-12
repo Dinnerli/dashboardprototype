c71445cbe25bc6a4453b22f11b403dac6c73a15c
Ashirwada Manamudali <91813126+zionashirwada@users.noreply.github.com>
2025-06-12 11:16:39 +0530

    feat: update tailwind.config.ts to define additional screen sizes for responsive design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,8 +14,16 @@ export default {
 			center: true,
 			padding: '2rem',
 			screens: {
-				'2xl': '1400px'
+				'2xl': '1400px',
+				'lg': '1200px'
 			}
+		},
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1200px',
+			'xl': '1280px',
+			'2xl': '1400px',
 		},
 		extend: {
 			fontFamily: {
